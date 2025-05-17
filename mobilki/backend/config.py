3903fe7a603ec2217f3b1b916396571501c60a0a
Marek Staniecki <maresta733@student.polsl.pl>
2025-05-17 14:24:41 +0200

    Dodano funkcję generowania raportów z eksportem PDF i analizą kategorii - Wprowadzono report_routes do generowania raportów finansowych. - Dodano funkcję generowania plików PDF przy użyciu ReportLab. - Zaimplementowano wizualizację wykresu kołowego dla kategorii wydatków. - Skonfigurowano zmienne środowiskowe dla bazy danych i tajnego klucza. - Poprawiono zwracany kod przy rejestracji nowego użytkownika

--- mobilki/backend/config.py
+++ mobilki/backend/config.py
@@ -1,8 +1,11 @@
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "bardzo_tajny_klucz")
-    MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017/PWA")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    MONGO_URI = os.environ.get("MONGO_URI")
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = 'Lax'
     PERMANENT_SESSION_LIFETIME = 60 * 60 * 24 * 7  # 7 dni
@@ -15,4 +18,3 @@ class ProductionConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
-    MONGO_URI = "mongodb://localhost:27017/PWA"
