045aa3c9e692716b601bb049a297642b5f553429
Marek Staniecki <maresta733@student.polsl.pl>
2025-05-18 13:15:38 +0200

    Dodanie wykresów i funkcji raportowania do strony głównej, aktualizacja zależności

--- mobilki/frontend/src/lib/auth.tsx
+++ mobilki/frontend/src/lib/auth.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:5000";
+const API_URL = "http://localhost:5000";
 
 export const login = async (data: { username: string; password: string }) => {
   const response = await axios.post(`${API_URL}/login`, data, {
