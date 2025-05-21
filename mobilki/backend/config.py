bb7165374a65fffa6c5ee75d36b597b8fa28b7e5
Marek Staniecki <maresta733@student.polsl.pl>
2025-05-21 21:29:16 +0200

    Dodanie FRONT_END_URL do konfiguracji środowiska i aktualizacja ustawienia CORS

--- mobilki/backend/config.py
+++ mobilki/backend/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
     MONGO_URI = os.environ.get("MONGO_URI")
+    FRONT_END_URL = os.environ.get("FRONT_END_URL")
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = 'Lax'
     PERMANENT_SESSION_LIFETIME = 60 * 60 * 24 * 7  # 7 dni
